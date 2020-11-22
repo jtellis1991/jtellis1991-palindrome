7ac8d621c7211f43a2df8d8e246d7af0bc4e1cb1
Josh Ellis <joshuaellis1991@gmail.com>
2020-11-22 19:34:39 +0000

    Initialize Repository

---
+++ index.js
@@ -0,0 +1,24 @@
+// Adds `reverse` to all strings.
+String.prototype.reverse = function() {
+  return Array.from(this).reverse().join("");
+}
+
+//Adds `blank` to all strings, checking if empey or whitespace
+String.prototype.blank = function() {
+  return !!(this.match(/^\s*$/g)); 
+}
+
+// Defines a Phrase object.
+function Phrase(content) {
+  this.content = content;
+
+  // Returns content processed for palindrome testing.
+  this.processedContent = function processedContent() {
+    return this.content.toLowerCase();
+  }
+
+  // Returns true if the phrase is a palindrome, false otherwise.
+  this.palindrome = function palindrome() {
+    return this.processedContent() === this.processedContent().reverse();
+  }
+}
